4dad858040940871cd37420efe66ee529863234b
kushal atreya <kushalatreya@live.com>
2019-01-15 15:53:49 +0200

    handleInput and saveSubmit function added for saving locally

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 
 export default class AddTodo extends Component {
+    state={
+        id : '',
+        todo : ''
+    };
 
+    handleInput = e => {
+    let target = e.target;
+    let name = target.name;
+    let value = target.value;
+    const _id = this.props.dataForNewTodo[this.props.dataForNewTodo.length - 1].id + 1;
+/*  console.log(this.props.dataForNewTodo[this.props.dataForNewTodo.length -1].id +1);*/ this.setState({[name]:value, id:_id})
+    console.log(target)
+    };
 
+    saveSubmit = (e)=>{
+    e.preventDefault()
+    this.props.addTodo(this.state)
+    }
 
   render() {
-
     return (
       <div>
-        <form>
+        <form onSubmit= {this.saveSubmit}>
         <label>Add ToDo:  </label>
-        <input type="text" placeholder= 'add TODO' onChange= {this.handleInput} />
+        <input type="text" name= 'todo' placeholder= 'add TODO' value={this.state.todo} onChange= {this.handleInput} />
         <button>Add</button><br/>
-       {/*  <button>Edit</button>
-        <button>View</button>
-        <button>Delete</button> */}
         </form>
       </div>
     )
