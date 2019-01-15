a6a21941d0c1119973a5764bcbb4d97fe8c3e78a
kushal atreya <kushalatreya@live.com>
2019-01-15 15:54:53 +0200

    event handler for view and delete button

--- src/components/ShowTodo.js
+++ src/components/ShowTodo.js
@@ -1,35 +1,27 @@
 /* This component is to map and show the todos saved in the state */
-import React, { Component } from 'react'
-import './showtodo.css'
+import React, { Component } from "react";
+import "./showtodo.css";
 
 export default class ShowTodo extends Component {
   render() {
     return (
       <div>
-            {this.props.dataToMap.map((element,index)=>{
-                return <div className="each-todo-container">
-                    <p>{element.id}</p>
-                    <p>{element.todo}</p>
-                    <button>Edit</button>
-                    <button
-                      onClick={() =>
-                        this.props.idSelector(element.id)
-                      }
-                    >
-                      View
-                    </button>
-                    <button
-                      onClick={() =>
-                        this.props.forDelete(element.id)
-                      }
-                    >
-                      Delete
-                    </button>
-                  </div>;
-            })
-            }
-
+        {this.props.dataToMap.map((element, index) => {
+          return (
+            <div className="each-todo-container" key={index}>
+              <p>{element.id}</p>
+              <p>{element.todo}</p>
+              <button>Edit</button>
+              <button onClick={() => this.props.idSelector(element.id)}>
+                View
+              </button>
+              <button onClick={() => this.props.forDelete(element.id)}>
+                Delete
+              </button>
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
